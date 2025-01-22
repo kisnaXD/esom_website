d5560324bcf4975b2de7f0fabc08d9bb7a53c182
kisnaXD <krishnagera93@gmail.com>
2025-01-23 00:20:50 +0530

    Events page done

--- teamscript.js
+++ teamscript.js
@@ -4,15 +4,9 @@ document.addEventListener("DOMContentLoaded", function() {
     const sidebar = document.getElementById('sidebar');
     toggleSidebarButton.addEventListener('click', () => {
         sidebar.classList.add('sidebar-open');
-        document.querySelector('.scroll-button').style.zIndex = '-1'
-        document.querySelector('.scroll-container').style.zIndex = '-1'
-        document.querySelector('.hero-text').style.zIndex = '-1'
     });
 
     closeSidebarButton.addEventListener('click', () => {
         sidebar.classList.remove('sidebar-open');
-        document.querySelector('.scroll-button').style.zIndex = '15'
-        document.querySelector('.scroll-container').style.zIndex = '10'
-        document.querySelector('.hero-text').style.zIndex = '10'
     });
 });
